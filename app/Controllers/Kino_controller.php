85a524d330f311b2a5d0c6c5cfd35e6eb72ce238
JakubKatrnak <katrnakjakub@gmail.com>
2021-04-08 16:26:33 +0200

    update form

--- app/Controllers/Kino_controller.php
+++ app/Controllers/Kino_controller.php
@@ -1,6 +1,7 @@
 <?php namespace App\Controllers;
 
 use \App\Models\Kino_model;
+use \App\Models\Nazev_model;
 
 class Kino_controller extends BaseController
 {
@@ -29,22 +30,47 @@ public function form()
 
 	if ( $this->ionAuth->loggedIn() ) {
 
-		$model = new Kino_model();
+		$nazev_model = new Nazev_model();
+		$film_model = new Kino_model();
 
 		if ($this->request->getMethod() === 'post' && $this->validate([
 				'nazev_filmu' => 'required|min_length[3]|max_length[255]',
 				'delka'  => 'required',
+				'jazyk'  => 'required',
 			]))
 		{
-			$model->save([
-				'nazev_filmu' => $this->request->getPost('nazev_filmu'),
+			$film_data = [
+				
+				'delka'  			=> $this->request->getPost('delka'),
+				'id_typ'			=> $this->request->getPost('typ'),
+				'id_zanr' 			=> $this->request->getPost('druh'),
+			];
 
-				'alternativni_filmu'  => $this->request->getPost('alternativni_filmu'),
-				'delka'  => $this->request->getPost('delka'),
-				'typ' => $this->request->getPost('typ'),
-				'druh' => $this->request->getPost('druh'),
-			]);
-	
+			$og_nazev_data = [			
+				
+				'nazev' 			=> $this->request->getPost('nazev_filmu'),
+				'jazyk'				=> $this->request->getPost('jazyk'),
+				'originalni'		=> 1,
+
+			];
+
+			if($this->request->getPost('alt_nazev') != null)
+			{
+				$alt_nazev_data = [			
+				
+					'nazev' 		=> $this->request->getPost('alt_nazev'),
+					'jazyk'			=> $this->request->getPost('jazyk_alt'),
+					'originalni'	=> 0,
+				];
+				
+				$nazev_model->insert($alt_nazev_data);
+			}
+			else
+
+
+			$film_model->insert($film_data);
+			$nazev_model->insert($og_nazev_data);
+
 			echo view('layout/header_loggedIn', ['title' => 'Položka přidáná!']);
 			echo view('content/form');
 			echo view('layout/footer');
@@ -52,7 +78,7 @@ public function form()
 		}
 		else
 		{
-				echo view('layout/header_loggedIn', ['title' => 'Přidej položku']);
+				echo view('layout/header_loggedIn', ['title' => 'Něco je špatně']);
 				echo view('content/form');
 				echo view('layout/footer');
 
